aac987dae06aebd323f3e53f0cc24fd32f20d813
Fatii59 <93334642+Fatii59@users.noreply.github.com>
2024-09-11 20:44:37 +0200

    post implementation

---
+++ Entities/Post.cs
@@ -0,0 +1,26 @@
+﻿namespace Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
+    public List<Comment> Comments { get; set; }
+
+
+    public Post(string title, string body, int userId)
+    {
+        Title = title;
+        Body = body;
+        UserId = userId;
+        Comments = new List<Comment>();
+    }
+
+    
+    public Post()
+    {
+        Comments = new List<Comment>();
+    }
+}
