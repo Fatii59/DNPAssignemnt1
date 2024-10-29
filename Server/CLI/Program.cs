18d29271ab38c6dbec498ae19006fbb0d9706850
Fatii59 <93334642+Fatii59@users.noreply.github.com>
2024-10-29 09:33:35 +0100

    comment out program

--- Server/CLI/Program.cs
+++ Server/CLI/Program.cs
@@ -10,8 +10,8 @@ IUserRepository userRepository= new UserFileRepository();
 ICommentRepository commentRepository= new CommentFileRepository();
 IPostRepository postRepository= new PostFileRepository();
 UserService userService = new UserService(userRepository);
-CommentService commentService = new CommentService(commentRepository);
-PostService postService = new PostService(postRepository);
+//CommentService commentService = new CommentService(commentRepository);
+//PostService postService = new PostService(postRepository);
 
-CliApp cliApp = new CliApp(userService, commentService, postService);
-await cliApp.StartAsync();
+//CliApp cliApp = new CliApp(userService, commentService, postService);
+//await cliApp.StartAsync();
