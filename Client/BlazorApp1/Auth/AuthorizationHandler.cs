18550b1a57acdf67492ec33c1c2dea023b451461
Fatii59 <93334642+Fatii59@users.noreply.github.com>
2024-11-18 22:01:25 +0100

    Plus these two

---
+++ Client/BlazorApp1/Auth/AuthorizationHandler.cs
@@ -0,0 +1,28 @@
+﻿
+namespace BlazorApp1.Auth;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class AuthorizationHandler : DelegatingHandler
+{
+    private readonly SimpleAuthProvider _authProvider;
+
+    public AuthorizationHandler(SimpleAuthProvider authProvider)
+    {
+        _authProvider = authProvider;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var authState = await _authProvider.GetAuthenticationStateAsync();
+        var token = authState.User.FindFirst("jwt")?.Value;
+
+        if (!string.IsNullOrEmpty(token))
+        {
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
---
+++ Client/BlazorApp1/Models/LoginResponse.cs
@@ -0,0 +1,9 @@
+﻿using DTOs;
+
+namespace BlazorApp1.Models;
+
+public class LoginResponse
+{
+    public string Token { get; set; }
+    public UserDTO User { get; set; }
+}
